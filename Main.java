750a5882d566104cc9ba17dbaeb5f2c2cf52c6d7
Maximillian Mahlke <themaxta@gmail.com>
2022-02-09 16:35:52 -0700

    added functionality for user entry

--- Main.java
+++ Main.java
@@ -1,10 +1,27 @@
 package com.company;
+import java.util.*;
 public class Main {
     public static void main(String[] args) {
         // write your code here
         for (int i = 1; i < 10; i++) {
             System.out.print(i);
             System.out.print(" and a .. ");
+
+
+
+
+
         }
+        Scanner sc= new Scanner(System.in); //System.in is a standard input stream
+        System.out.print("Enter a string: ");
+        String str= sc.nextLine();              //reads string
+        System.out.print("You have entered: "+str);
+
+        System.out.println("I forgot how to code");
+
     }
+
+
+
+
 }
